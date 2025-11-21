22ff883d350ce3432a2d976a27cb70e6226d3c3d
infex1rn <infexgee@gmail.com>
2025-11-22 00:38:16 +0100

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+title INDEVSTUDIO ULTRA DEBLOAT
+echo Starting INDEVSTUDIO Ultra Debloat...
+echo.
+powershell -NoProfile -ExecutionPolicy Bypass -File "INDEVSTUDIO-ULTRA-DEBLOAT.ps1"
+pause
